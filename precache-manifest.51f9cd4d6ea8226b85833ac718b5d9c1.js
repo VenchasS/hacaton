f147f767f21ef9c29e549334499bb7ce898753d3
VenchasS <49115035+VenchasS@users.noreply.github.com>
2021-11-21 03:37:22 +0500

    Add files via upload

---
+++ precache-manifest.51f9cd4d6ea8226b85833ac718b5d9c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0017d38c4a622c143dbf3e218bea1236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e605ed628eedb028f52",
+    "url": "/static/css/2.4f26e51f.chunk.css"
+  },
+  {
+    "revision": "70d1337ce72ad511406f",
+    "url": "/static/css/main.3f950bff.chunk.css"
+  },
+  {
+    "revision": "7e605ed628eedb028f52",
+    "url": "/static/js/2.c96b7b15.chunk.js"
+  },
+  {
+    "revision": "22846d55ba0af4676140da083bb080fb",
+    "url": "/static/js/2.c96b7b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483165dc6604ebc92462",
+    "url": "/static/js/3.b4516daa.chunk.js"
+  },
+  {
+    "revision": "70d1337ce72ad511406f",
+    "url": "/static/js/main.716a25a8.chunk.js"
+  },
+  {
+    "revision": "c7a9e6ddb37b9fd18819",
+    "url": "/static/js/runtime-main.ce6d0117.js"
+  },
+  {
+    "revision": "a0778038892fe89ad12de0eb230cc85c",
+    "url": "/static/media/2d2ff9a5b3934dab91273eb277df9be2_00000.a0778038.jpg"
+  }
+]);
